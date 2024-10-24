5f78c410b25adb0bad113d7e4a11195af1ed91c2
horadmard <iranradmard1@gmail.com>
2024-10-24 12:22:34 +0330

    Done, proxy pass request solved

--- auth-interface/config.ts
+++ auth-interface/config.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const BASE_URL = "http://localhost:3000/auth"
+export const BASE_URL = "https://localhost:443/auth"
 
 export const mainApiInstance = axios.create(
     {
